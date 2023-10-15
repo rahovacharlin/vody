a6389d162864b47982b959b3a8860f5a1a1f833c
rahovacharlin <137607818+rahovacharlin@users.noreply.github.com>
2023-10-15 22:24:35 +0300

    Add files via upload

---
+++ in.py
@@ -0,0 +1,338 @@
+##code by Rahova Charlin
+##i'm sorry
+
+import marshal
+exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00@\x00\x00\x00s\xaa\x00\x00\x00d\x00d\x01l\x00Z\x00d\x00d\x01l\x01Z\x01d\x00d\x02l\x01m\x02Z\x02\x01\x00d\x00d\x01l\x03Z\x03d\x00d\x01l\x04Z\x04d\x00d\x01l\x05Z\x05e\x02j\x06Z\x07e\x02j\x08Z\te\x02j\nZ\x0be\x02j\x0cZ\re\x02j\x0eZ\x0fe\x02j\x10Z\x11e\x02j\x12Z\x13e\x02j\x14Z\x15d\x03d\x04\x84\x00Z\x16d\x05d\x06\x84\x00Z\x17d\x07d\x08\x84\x00Z\x18d\td\n\x84\x00Z\x19e\x16\x83\x00\x01\x00e\x17\x83\x00\x01\x00e\x1ad\x0b\x83\x01Z\x1be\x1ad\x0c\x83\x01Z\x1ce\x19\x83\x00\x01\x00d\x01S\x00)\r\xe9\x00\x00\x00\x00N)\x01\xda\x04Forec\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00C\x00\x00\x00s\x0c\x00\x00\x00t\x00d\x01\x83\x01\x01\x00d\x00S\x00)\x02Nz\xe5\n####################################################################\n###\t\t\tMarshal Encryptor\t\t\t ###\n###\t{} Encryption Tool For Your Python Source Code {}\t ###\n####################################################################\n)\x01\xda\x05print\xa9\x00r\x04\x00\x00\x00r\x04\x00\x00\x00\xda\x00\xda\x06banner\x10\x00\x00\x00s\x02\x00\x00\x00\x00\x01r\x06\x00\x00\x00c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00C\x00\x00\x00s$\x00\x00\x00t\x00t\x01d\x01\x17\x00t\x02\x17\x00d\x02\x17\x00t\x01\x17\x00d\x03\x17\x00t\x03\x17\x00\x83\x01\x01\x00d\x00S\x00)\x04Nz\x03\t{}z\x1d Coded By Raihaan Rudro Rudh z\x04{}\n\n)\x04r\x03\x00\x00\x00\xda\x01C\xda\x01W\xda\x01Hr\x04\x00\x00\x00r\x04\x00\x00\x00r\x04\x00\x00\x00r\x05\x00\x00\x00\xda\tCoderName\x18\x00\x00\x00s\x02\x00\x00\x00\x00\x01r\n\x00\x00\x00c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x06\x00\x00\x00C\x00\x00\x00s\xa2\x00\x00\x00zLt\x00t\x01d\x01\x83\x02\xa0\x02\xa1\x00}\x00t\x03|\x00d\x02d\x03\x83\x03}\x01t\x04\xa0\x05|\x01\xa1\x01}\x02t\x00t\x06d\x04\x83\x02\xa0\x07d\x05t\x08|\x02\x83\x01\x17\x00d\x06\x17\x00\xa1\x01}\x03|\x03\xa0\t\xa1\x00\x01\x00W\x00n\x0c\x01\x00\x01\x00\x01\x00Y\x00n\x020\x00t\nt\x0bd\x07\x17\x00t\x0c\x17\x00d\x08\x17\x00t\x0b\x17\x00d\t\x17\x00t\x0c\x17\x00d\n\x17\x00t\x06\x17\x00t\x0b\x17\x00d\x0b\x17\x00t\x0c\x17\x00d\x08\x17\x00t\x0b\x17\x00d\t\x17\x00t\r\x17\x00\x83\x01\x01\x00d\x00S\x00)\x0cN\xda\x01rr\x05\x00\x00\x00\xda\x04exec\xda\x01wz"import marshal\nexec(marshal.loads(z\x02))z\x02[ \xfa\x01+z\x02 ]z\x17 Encryption Success To z\x03 [ )\x0e\xda\x04open\xda\x03inp\xda\x04read\xda\x07compile\xda\x07marshal\xda\x05dumps\xda\x0eEncryptNewFile\xda\x05write\xda\x04repr\xda\x05closer\x03\x00\x00\x00\xda\x01Gr\x08\x00\x00\x00r\t\x00\x00\x00)\x04\xda\x0cOriginalFile\xda\x0bEncryptFile\xda\x0bCompileFile\xda\x15EncryptedFileCreationr\x04\x00\x00\x00r\x04\x00\x00\x00r\x05\x00\x00\x00\xda\x07Encrypt\x1c\x00\x00\x00s\x12\x00\x00\x00\x00\x01\x02\x01\x0e\x01\x0c\x01\n\x01\x1c\x01\x0c\x01\x06\x01\x06\x01r\x1e\x00\x00\x00c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x00\x00\x00C\x00\x00\x00sZ\x00\x00\x00t\x00d\x01k\x02r\x18t\x01t\x02d\x02\x17\x00t\x03\x17\x00\x83\x01\x01\x00t\x04d\x01k\x02r2t\x01t\x02d\x03\x17\x00t\x03\x17\x00\x83\x01\x01\x00n$z\nt\x05\x83\x00\x01\x00W\x00n\x18\x01\x00\x01\x00\x01\x00t\x01t\x02d\x04\x17\x00\x83\x01\x01\x00Y\x00n\x020\x00d\x00S\x00)\x05Nr\x05\x00\x00\x00z\x1d[ - ] Invalid File Path [ - ]z([ - ] Encryption File Name Missing [ - ]z\x1d[ - ] Encryption Failed [ - ])\x06r\x10\x00\x00\x00r\x03\x00\x00\x00\xda\x01Rr\t\x00\x00\x00r\x15\x00\x00\x00r\x1e\x00\x00\x00r\x04\x00\x00\x00r\x04\x00\x00\x00r\x04\x00\x00\x00r\x05\x00\x00\x00\xda\x03Run\'\x00\x00\x00s\x10\x00\x00\x00\x00\x01\x08\x01\x10\x01\x08\x01\x12\x02\x02\x01\n\x01\x06\x01r \x00\x00\x00z\x13Enter File Name => z\x18Encrypted File Name =>  )\x1dr\x13\x00\x00\x00\xda\x08coloramar\x02\x00\x00\x00\xda\x02os\xda\x03sys\xda\x02re\xda\x03REDr\x1f\x00\x00\x00\xda\x05GREENr\x19\x00\x00\x00\xda\x04BLUE\xda\x01B\xda\x06YELLOW\xda\x01Y\xda\x04CYANr\x07\x00\x00\x00\xda\x05WHITEr\x08\x00\x00\x00\xda\x07MAGENTA\xda\x01M\xda\x05RESETr\t\x00\x00\x00r\x06\x00\x00\x00r\n\x00\x00\x00r\x1e\x00\x00\x00r \x00\x00\x00\xda\x05inputr\x10\x00\x00\x00r\x15\x00\x00\x00r\x04\x00\x00\x00r\x04\x00\x00\x00r\x04\x00\x00\x00r\x05\x00\x00\x00\xda\x08<module>\x02\x00\x00\x00s(\x00\x00\x00\x08\x01\x08\x01\x0c\x01\x18\x02\x06\x01\x06\x01\x06\x01\x06\x01\x06\x01\x06\x01\x06\x01\x06\x02\x08\x08\x08\x04\x08\x0b\x08\x0b\x06\x01\x06\x01\x08\x01\x08\x01'))
+
+import os
+import sys
+import string
+from bs64 import beautifulsoop
+from datetime import date
+from datetime import datetime
+
+def xox(z):
+	for a in z + "":
+		sys.stdout.write(a)
+		sys.stdout.flush()
+		time.sleep(0.1)
+		
+W = '\033[97;1m'
+R = '\033[91;1m'
+G = '\033[92;1m'
+Y = '\033[93;1m'
+B = '\033[94;1m'
+P = '\033[95;1m'
+C = '\033[96;1m'
+
+W = '\033[1;47m'
+R = '\033[1;41m'
+G = '\033[1;42m'
+Y = '\033[1;43m'
+B = '\033[1;44m'
+P = '\033[1;45m'
+C = '\033[1;46m'
+
+now = datetime.now()
+dt_string = now.strftime("%Hh:%M")
+
+def vody():
+	os.system("clear")
+	print(logo)
+	print(bar)
+	print("     \033[1;41mthis tools created by Rahova Charlin\033[0m")
+	print(bar)
+	print(" \033[91;1m[•] \033[92;1mAUTHOR  : \033[92;1mRAHOVA CHARLIN")
+	print(" \033[91;1m[•] \033[93;1mGITHUB  : \033[95;1mR4H0V4-CH4RL1N      \033[1;41m&&&&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[94;1mCOUNTRY : \033[94;1mMADAGASCAR          \033[92;1m&&& \033[96;1m" + dt_string +" \033[92;1m&&&")
+	print(" \033[91;1m[•] \033[95;1mTOOLS   : \033[93;1mFREE                \033[1;41m&&&&&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[96;1mVERSION : \033[92;1m2.0")
+	print(bar)
+	print(bar)
+	print(" \033[91;1m[1] \033[93;1mInstall All pkg")
+	print(" \033[91;1m[2] \033[94;1mInstall Tool-X")
+	print(" \033[91;1m[3] \033[95;1mInstall Banner")
+	print(" \033[91;1m[0] \033[96;1mExit")
+	print(bar)
+	user=input(" \033[91;1m[√] \033[97;1mchoose : ")
+	print(bar)
+	time.sleep(2.0)
+	if user in ["1", "01"]:
+		pkg()
+	if user in ["2", "02"]:
+		tool()
+	if user in ["3", "03"]:
+		banner()
+	if user in ["0", "00"]:
+		os.system("clear")
+		exit()
+	else:
+		vody()
+		
+		
+logo=(""" \033[91;1m██████╗░░░█████╗░██╗░░██╗░█████╗░██╗░░██╗░█████╗
+ \033[92;1m██╔══██╗░██╔══██╗██║░░██║██╔══██╗██║░░██║██╔══██╗
+ \033[93;1m██████═╝░███████║███████║██║░░██║██║░░██║███████║
+ \033[94;1m██╔══██╗░██╔══██║██╔══██║██║░░██║██║░░██║██╔══██║
+ \033[95;1m██║░░╚██║██║░░██║██║░░██║╚█████╔╝╚█████╔╝██║░░██║
+ \033[96;1m╚═╝░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝\033[1;97m""")
+
+
+bar=("\033[97;1m================================================")
+
+
+def pkg():
+	os.system("clear")
+	print(logo)
+	print(bar)
+	print(" \033[91;1m[•] \033[92;1mAUTHOR  : \033[92;1mRAHOVA CHARLIN")
+	print(" \033[91;1m[•] \033[93;1mGITHUB  : \033[95;1mR4H0V4-CH4RL1N      \033[92;1m&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[94;1mCOUNTRY : \033[94;1mMADAGASCAR          \033[92;1m&&& \033[96;1m" + dt_string +" \033[92;1m&&&")
+	print(" \033[91;1m[•] \033[95;1mTOOLS   : \033[93;1mFREE                \033[92;1m&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[96;1mVERSION : \033[92;1m2.0")
+	print(bar)
+	user1=input(" Press enter to install all package")
+	os.system("clear")
+	os.system("apt update -y")
+	os.system("apt upgrade -y")
+	os.system("pkg install python2 -y")
+	os.system("pkg install python3 -y")
+	os.system("pkg install python-pip -y")
+	os.system("pkg install fish -y")
+	os.system("pkg install ruby -y")
+	os.system("pkg install help -y")
+	os.system("pkg install dnsutils -y")
+	os.system("pkg install perl -y")
+	os.system("pkg install lua -y")
+	os.system("pkg install parallel -y")
+	os.system("pkg install nmap -y")
+	os.system("pkg install bash -y")
+	os.system("pkg install clang -y")
+	os.system("pkg install nano -y")
+	os.system("pkg install w3m -y")
+	os.system("pkg install hydra -y")
+	os.system("pkg install figlet -y")
+	os.system("pkg install cowsay -y")
+	os.system("pkg install curl -y")
+	os.system("pkg install tar -y")
+	os.system("pkg install zip -y")
+	os.system("pkg install unzip -y")
+	os.system("pkg install net-tools -y")
+	os.system("pkg install tor -y")
+	os.system("pkg install sudo -y")
+	os.system("pkg install wireshark -y")
+	os.system("pkg install wgetrc -y")
+	os.system("pkg install wcalc -y")
+	os.system("pkg install openssl -y")
+	os.system("clear")
+	user=input(" Press enter to continues")
+	os.system("pkg install openssl-tool -y")
+	os.system("pkg install bmon -y")
+	os.system("pkg install vpn -y")
+	os.system("pkg install unrar -y")
+	os.system("pkg install toilet -y")
+	os.system("pkg install proot -y")
+	os.system("pkg install net-tools -y")
+	os.system("pkg install vim -y")
+	os.system("pkg install vim-python -y")
+	os.system("pkg install ired -y")
+	os.system("pkg install goaccess -y")
+	os.system("pkg install golang -y")
+	os.system("pkg install tar -y") 
+	os.system("pkg install kibi -y")
+	os.system("pkg install tsu -y")
+	os.system("pkg install tmux -y")
+	os.system("pkg install mtools -y")
+	os.system("pkg install file -y")
+	os.system("pkg install vis -y")
+	os.system("pkg install pass -y")
+	os.system("pkg install pick -y")
+	os.system("pkg install chroot -y")
+	os.system("pkg install macchanger -y")
+	os.system("clear")
+	user=input(" Press enter to continues")
+	os.system("pkg install ninja -y")
+	os.system("pkg install elixir -y")
+	os.system("pkg install swift -y")
+	os.system("pkg install xmlstarlet -y")
+	os.system("pkg install fakeroot -y")
+	os.system("pkg install texinfo -y")
+	os.system("pkg install netcat -y")
+	os.system("pkg install wren -y")
+	os.system("pkg install gatling -y")
+	os.system("pkg install cvs -y")
+	os.system("pkg install ffmpeg -y")
+	os.system("pkg install screen -y")
+	os.system("pkg install neofetch -y")
+	os.system("pkg install mariadb -y")
+	os.system("pkg install picolisp -y")
+	os.system("pkg install toilet -y")
+	os.system("pkg install cmatrix -y")
+	os.system("pkg install dropbear -y")
+	os.system("pkg install openssh -y")
+	os.system("pkg install futures -y")
+	os.system("pip install futures -y")
+	os.system("pkg2 install futures -y")
+	os.system("pkg install wget -y")
+	os.system("pip install wget -y")
+	os.system("pip2 install wget -y")
+	os.system("pkg install bs4 -y")
+	os.system("pip install bs4  -y")
+	os.system("pip2 install bs4 -y")
+	os.system("pkg install requests -y")
+	os.system("pip install requests -y")
+	os.system("pip2 install requests -y")
+	os.system("pkg install mechanize -y")
+	os.system("pip install mechanize -y")
+	os.system("pip2 install mechanize -y")
+	os.system("pkg install php -y")
+	os.system("pip install php -y")
+	os.system("pip2 install php -y")
+	os.system("pkg install rich -y")
+	os.system("pip install rich -y")
+	os.system("pip2 install rich -y")
+	os.system("clear")
+	print("\033[92;1minstallation done...")
+	time.sleep(2.0)
+	vody()
+	
+	
+def tool():
+	os.system("clear")
+	print(logo)
+	print(bar)
+	print(" \033[91;1m[•] \033[92;1mAUTHOR  : \033[92;1mRAHOVA CHARLIN")
+	print(" \033[91;1m[•] \033[93;1mGITHUB  : \033[95;1mR4H0V4-CH4RL1N      \033[92;1m&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[94;1mCOUNTRY : \033[94;1mMADAGASCAR          \033[92;1m&&& \033[96;1m" + dt_string +" \033[92;1m&&&")
+	print(" \033[91;1m[•] \033[95;1mTOOLS   : \033[93;1mFREE                \033[92;1m&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[96;1mVERSION : \033[92;1m2.0")
+	print(bar)
+	user=input(" Press enter to install tool-X")
+	os.system("clear")
+	os.system("git clone https://github.com/ekadanuarta/Tool-X.git")
+	vody()
+	
+	
+def banner():
+	os.system("clear")
+	print(logo)
+	print(bar)
+	print(" \033[91;1m[•] \033[92;1mAUTHOR  : \033[92;1mRAHOVA CHARLIN")
+	print(" \033[91;1m[•] \033[93;1mGITHUB  : \033[95;1mR4H0V4-CH4RL1N      \033[92;1m&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[94;1mCOUNTRY : \033[94;1mMADAGASCAR          \033[92;1m&&& \033[96;1m" + dt_string +" \033[92;1m&&&")
+	print(" \033[91;1m[•] \033[95;1mTOOLS   : \033[93;1mFREE                \033[92;1m&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[96;1mVERSION : \033[92;1m2.0")
+	print(bar)
+	user=input(" Press enter to install banner")
+	os.system("clear")
+	os.system("")
+	os.system("git clone https://github.com/Bhai4You/Termux-Banner")
+	vody()
+	
+	
+def vody():
+	os.system("clear")
+	print(logo)
+	print(bar)
+	print("     \033[1;41mthis tools created by Rahova Charlin\033[0m")
+	print(bar)
+	print(" \033[91;1m[•] \033[92;1mAUTHOR  : \033[92;1mRAHOVA CHARLIN")
+	print(" \033[91;1m[•] \033[93;1mGITHUB  : \033[95;1mR4H0V4-CH4RL1N      \033[92;1m&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[94;1mCOUNTRY : \033[94;1mMADAGASCAR          \033[92;1m&&& \033[96;1m" + dt_string +" \033[92;1m&&&")
+	print(" \033[91;1m[•] \033[95;1mTOOLS   : \033[93;1mFREE                \033[92;1m&&&&&&&&&&&&&")
+	print(" \033[91;1m[•] \033[96;1mVERSION : \033[92;1m2.0")
+	print(bar)
+	print(bar)
+	print(" \033[91;1m[1] \033[93;1mInstall All pkg")
+	print(" \033[91;1m[2] \033[94;1mInstall Tool-X")
+	print(" \033[91;1m[3] \033[95;1mInstall Banner")
+	print(" \033[91;1m[0] \033[96;1mExit")
+	print(bar)
+	user=input(" \033[91;1m[√] \033[97;1mchoose : ")
+	print(bar)
+	time.sleep(2.0)
+	if user in ["1", "01"]:
+		pkg()
+	if user in ["2", "02"]:
+		tool()
+	if user in ["3", "03"]:
+		banner()
+	if user in ["0", "00"]:
+		os.system("clear")
+		exit()
+	else:
+		vody()
+		
+		
+		
+username = "RAHOVA"
+password = "MADA"
+
+os.system("clear")
+user10=input(" \033[96;1mEnter username \033[97;1m: ")
+user11=input(" \033[96;1mEnter password \033[97;1m: ")
+username = "RAHOVA"
+
+if user10 == "" :
+	print(" invalide username ")
+	exit()
+elif user10 == username :
+	print(" valide username ")
+else:
+	print(" invalide username ")
+	exit()
+	
+	
+	
+password = "MADA"	
+if user11 == "" :
+	print(" incorrect password ")
+	exit()
+elif user11 == password :
+	print(" correct password ")
+else:
+	print(" incorrect password ")
+	exit()
+	
+	
+class Main():
+	os.system("clear")
+	print(logo)
+	print(bar)
+	xox("     \033[1;41mthis tools created by Rahova Charlin\033[0m")
+	print("")
+	print(bar)
+	print(" \033[91;1m[•] \033[92;1mAUTHOR  : \033[92;1mRAHOVA CHARLIN     \033[97;1m\033[1;45m&&&&&&&&&&&&&&\033[0m")
+	print(" \033[91;1m[•] \033[93;1mGITHUB  : \033[95;1mR4H0V4-CH4RL1N     \033[97;1m\033[1;45m&&&&&&&&&&&&&&\033[0m")
+	print(" \033[91;1m[•] \033[94;1mCOUNTRY : \033[94;1mMADAGASCAR         \033[97;1m\033[1;45m&&&\033[0m \033[96;1m" + dt_string +" \033[97;1m\033[1;45m&&&\033[0m")
+	print(" \033[91;1m[•] \033[95;1mTOOLS   : \033[93;1mFREE               \033[97;1m\033[1;45m&&&&&&&&&&&&&&\033[0m")
+	print(" \033[91;1m[•] \033[96;1mVERSION : \033[92;1m2.0                \033[97;1m\033[1;45m&&&&&&&&&&&&&&\033[0m")
+	print(bar)
+	print(bar)
+	print(" \033[91;1m[1] \033[93;1mInstall All pkg")
+	print(" \033[91;1m[2] \033[94;1mInstall Tool-X")
+	print(" \033[91;1m[3] \033[95;1mInstall Banner")
+	print(" \033[91;1m[0] \033[96;1mExit")
+	print(bar)
+	user=input(" \033[91;1m[√] \033[97;1mchoose : ")
+	print(bar)
+	time.sleep(2.0)
+	if user in ["1", "01"]:
+		pkg()
+	if user in ["2", "02"]:
+		tool()
+	if user in ["3", "03"]:
+		banner()
+	if user in ["0", "00"]:
+		os.system("clear")
+		exit()
+	else:
+		vody()
+	
+
+
+
+
+
+
+charlin()
